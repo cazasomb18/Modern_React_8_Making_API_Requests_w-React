ceb13a53e12bcd3f5249a36c2dc91e66ea28d568
claytonschoonoversmith@gmail.com <claytonschoonoversmith@gmail.com>
2020-12-14 10:33:40 -0600

    files set up

---
+++ js/app.js
@@ -0,0 +1,11 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//Fetching Data
+	//Overview
+		//once user enters search term and hit enter, we will then make a request to the Unsplash API
+		//Unsplash API return JSON records that contain info about a bunch of differentr 
+		//JSON will then be used to render a big list of images
+		//We will then organize these rendered images as a flat list on the screen
+
+	//Unsplash API steps
+		//Register as a developer
+		//
