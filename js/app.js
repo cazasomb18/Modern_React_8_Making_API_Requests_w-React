07a40f83ccb68e2e574bc13909001db71fdf8af3
claytonschoonoversmith@gmail.com <claytonschoonoversmith@gmail.com>
2020-12-14 18:09:25 -0600

    removed non-relevant file from diagrams

--- js/app.js
+++ js/app.js
@@ -180,124 +180,83 @@
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-//RenderingLists
-	//We want to create a new component whose sole function is to render the list:
+//GRID CSS
+	//CSS will only take us so far - we're going to have to learn about a NEW REACT FEATURE!
 
-		//1 - in components directory create a new file called ImageList.js
-		//2 - import react, create arrow function that renders a div w/ text 'ImageList', export ImageList:
-		import React from 'react';
-		const ImageList = () => {
-			return <div>ImageList</div>	
+	//THE CSS GRID SYSTEM - HOW TO TO IMPLEMENT IN JSX:
+		<div style="display: grid"></div>
+		//to get a better understanding of what's going on we're going to add in some CSS to implement
+		//the grid:
+
+	//1 - Create a new file called ImageList.css in components directory:
+
+	//2 - Add import statement in ImageList.js for ImageList.css and add className="image-list" in <div>
+		import './ImageList.css';
+		<div className="image-list"></div>
+
+	//3 - in ImageList.css: add the following rules to image-list class:
+		// display: grid;
+		// grid-template-columns: repeat(auto-fill, minmax (250px, 1fr));
+			//repeat --> creates a set # of columns
+			//auto-fill --> CSS rule will automatically decide how many columns to insert
+			//minmax (250px 1fr) --> each column will be at least 250px wide and 
+				//1fr --> every column we create, we want them to all be equally sized
+
+	//4 - also add this rule to all img tags in class image-list (.image-list img { rule:whatever; })
+		// width: 250px (sets width to 250px)
+
+	//5 - Go inside chrome console, click on elements tab to see the dotted lines for the 'grid slots'
+		//Grid-CSS will take each element and shove them into a different spot in that grid
+		//Each image is shoved up against the other, doesn't look nice, so let's add a grid-gap rule
+			// grid-gap: 10px
+				//adds a 10px boundary between the grid boundaries
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//Issues with Grid CSS
+	//Focus: how can we get these images to fit up against each other vertically? (remove vertical whitespace)
+		//doing this with grid-css alone is not very easy
+
+	//Imperfect solution:  add rule grid-auto-rows: 200px; to image-list class
+		//Now the vertical spacing is better but some of the images are taller than the assigned area
+
+	//To fix this: we can use grid-auto-rows: span 3; however, this will apply to each image and won't look right
+		//For every individual image we need to:
+			//Determine it's individual height
+			//Then use it's height to automatically determine the amount of grid-auto-row: span it needs
+
+			//We'll have to use the console and css to play around with these value
+			//Cannot be solved via css alone, so we'll ahve to write some js and this is why we'll need React.
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//Creating an Image Card Component
+
+	//To solve image spacing issue we have the following objectives:
+		//1 - Create a new react component responsible for rendering one single image at a time
+		//2 - once image is rendered this comp will figure out how large image is and adjust grid-row-end rule
+
+	//1 - Create ImageCard.js in components directory, make functional comp that returns an image wrapped in a div
+		const ImageCard = () => {
+			return(
+				 <div>
+				 	<img />
+				 </div>
+			);
 		};
-		export default ImageList;
-		/*3 - replace <div>Found: {this.state.term.length}</div> with our new component:*/ <ImageList/>
-		/*4 - Don't forget to import the ImageList component!: */ import ImageList from './ImageList';
+	//2 - add alt and url props to img tag:
+		<img src={image.urls.regular} alt={image.alt_description}/>
 
-	//NOW - we want to communicate the list of images down to the image list (parent->child using props)
-		//in App.js add the empty array we created in state for images on a new image prop in ImageList comp:
-			<ImageList images={this.state.images}/>
-		//in ImageList.js add props to ImageList arrow function as an argument
-			const ImageList = (props) => {
-				console.log(props.images);
-				/*log props.images to make sure we have the data we want*/
-			};
+	//3 - ImageList.js -> import ImageCard, and replace <img> tag with <ImageCard/> component w/ only the key prop:
+		<ImgCard key={id}/>
 
-
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-//Review of Map Statements
-	const nums = [0,1,2,3,4];
-
-	const newNums = [];
-
-	for (let i = 0; i < nums.length; i++) {
-	  newNums.push(nums[i] * 10);
-	};
-	/*THIS BLOCK ^^^^*/
-
-	nums.map(num => num * 10);
-	/*DOES THE SAME THING AS THIS*/
-		//Without altering the original data (as we cannot do via props system!)
-
-	nums.map(num => <div>{num}</div>);/*is how we'll reference this in JSX*/
-
-
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-//Rendering Lists of Components
-	//GOAL: use .map() JS function to return a list of images from our ImageList component
-
-	//1 - map props.images in ImageList.js:
-	const images = props.images.map((image) => {
-		return <img src={image.urls.regular} />
-	});
-
-	return <div>{images}</div>
-
-
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-//We now have a list-key warning from React:
-	//Warning: Each child should have a unique 'key' prop
-	//Let's say we have the following:
-
-	//TO DO LIST  						RENDERED LIST
-	//{id: 1, task: 'Take out trash'}	<div>Take Out Trash</div> 
-	//{id: 2, task: 'Wash the car'}		<div>Wash the car</div> 
-	//{id: 3, task: 'Walk the dog'}		<div>Walk the dog</div> 
-
-	//Now let's say someone wants to add another item to the TODO list:
-
-	//{id: 4, task: 'Mow the lawn'}		<div>Mow the lawn</div>
-		//So then React will rerender the application
-
-	//What happens is this
-
-	//TO DO LIST  						RENDERED LIST 				currently in the DOM
-	//{id: 1, task: 'Take out trash'}	<div>Take Out Trash</div> 	<div>Take Out Trash</div>
-	//{id: 2, task: 'Wash the car'}		<div>Wash the car</div> 	<div>Wash the car</div>
-	//{id: 3, task: 'Walk the dog'}		<div>Walk the dog</div> 	<div>Walk the dog</div>
-	//{id: 4, task: 'Mow the lawn'}		<div>Mow the lawn</div>
-
-		//In Memory, react will take list of Rendered Items, and Compare them to what's in the DOM;
-			//React WANTS to not render items that are already in the DOM
-				//In order to allow React to do this we need to add a 'key' prop to the list
-
-	//Keys enable react to effectively compare what it's rendered to what's in the DOM
-		//When React checks what it's rendered against the DOM it will grab that key for comparison
-		//to render any updates to lists.
-
-	//Every item in the list will eventually be rendered as an element in the DOM.
-	//Whenever we use a 'key' we try to use a value that will correspond to a distinct element
-	//that will be rendred in the DOM.
-		//When we're fetching data from an outside API and we're getting items that we want to show 
-		//as a list it almost always ends up being the ID property ends up being that unique key value.
-
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-//Implementing Keys in Lists
-
-	//Find the root corresponding JSX tag that is being returned from out map statement (<img/>):
-		return <image key={image.id} src={image.urls.regular}  /> /*<--like so*/
-		//add {image.id} tokey prop
-
-		//To get rid of alt"" warning we can also add image.alt_description to that prop:
-		return <image key={image.id} src={image.urls.regular} alt={image.alt_description} /> /*<--like so*/
-
-		//To clean up our code a little bit, let's destructure all the props we want from the image obj:
-		const images = props.images.map(({ id, alt_description, urls }) => {
-			return <image key={id} src={urls.regular} alt={alt_description} />
+	//4 - Also we are no longer destructuring the args so we change the map back to this:
+		const images = props.images.map((image) => {
+			return <ImageCard key={image.id} image={image}/>
+			//5.b - additionally the image to ImageCard as the image prop  
 		});
 
-		//at this point we now have an MVP app, and have completed v1
-
-
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-//Coding Exercise 7: Practing List Building
-	//Assign Key prop to each li, which will probably give you the ID to the user
